4e29edef514c1d5f8856454e3cecd286b43cabc2
Fabio Bara <bara@MacBook-Pro-de-Fabio.local>
2022-03-17 13:56:21 -0300

    Add unit test

--- src/test/java/com/neginet/PeopleApp/AppTest.java
+++ src/test/java/com/neginet/PeopleApp/AppTest.java
@@ -1,5 +1,14 @@
 package com.neginet.PeopleApp;
 
+import java.util.List;
+
+import com.neginet.people.domain.CommonNames;
+import com.neginet.people.domain.People;
+import com.neginet.people.domain.Person;
+import com.neginet.people.repository.Repository;
+import com.neginet.people.repository.impl.FileSystemRespoitory;
+import com.neginet.people.service.Service;
+import com.neginet.people.service.impl.ServiceImpl;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -11,29 +20,61 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+	private static Repository repository;
+	private static Service service;
+		
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
+    	
+    	String filePath = AppTest.class.getClassLoader().getResource("coding-test-data-dev.txt").getFile();
+    	repository = new FileSystemRespoitory(filePath);
+    	service = new ServiceImpl(repository);
+    	
         return new TestSuite( AppTest.class );
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
+    public void testCardinality() {
+    	
+    	People cardinality = service.getCardinality();
+		
+    	assertTrue(cardinality.getUniqueFullNamesSize() == 5 );
+    	assertTrue(cardinality.getUniqueLastNamesSize() == 4 );
+    	assertTrue(cardinality.getUniqueFirstNamesSize() == 3 );
+    	
+    }	
+    public void testCommonLastNames() {
+    	
+    	List<CommonNames> names = service.getMostCommonLastNames();
+    	
+    	assertTrue( names.size() == 4  );
+    	
+    	assertTrue( names.get(0).getOccurrenceQuantities() == 2  );
+    	assertTrue( names.get(1).getOccurrenceQuantities() == 1  );
+    	assertTrue( names.get(2).getOccurrenceQuantities() == 1  );
+    	assertTrue( names.get(3).getOccurrenceQuantities() == 1  );
+    }	
+    public void testCommonFirstNames() {
+    	
+    	List<CommonNames> names = service.getMostCommonFirstNames();
+    	
+    	assertTrue( names.size() == 3  );
+    	
+    	assertTrue( names.get(0).getOccurrenceQuantities() == 3  );
+    	assertTrue( names.get(1).getOccurrenceQuantities() == 1  );
+    	assertTrue( names.get(2).getOccurrenceQuantities() == 1  );
+    }	
+    
+    public void testModifiedNames() {
+    	
+    	List<Person> people = service.getCardinality().getPeople();
+    	People modified = service.getModifiedNames();
+    	
+    	people.forEach(row -> assertTrue(modified.isItANewName(row)));
+    	
+    }	
 }
